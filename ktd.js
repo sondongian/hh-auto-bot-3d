272be6f5b491a4737a90e58d8dd19ca9d0d29168
sondongian <77824725+sondongian@users.noreply.github.com>
2025-05-16 12:15:15 +0700

    Update ktd.js

--- ktd.js
+++ ktd.js
@@ -1,4 +1,4 @@
-const DOMAIN = 'https://hoathinh3d.cam';
+const DOMAIN = 'https://hoathinh3d.site';
 const ACTION_URL = DOMAIN + '/wp-json/hh3d/v1/action';
 const HH3D_AJAX_URL = DOMAIN + '/wp-content/themes/halimmovies-child/hh3d-ajax.php'
 const ADMIN_AJAX_URL = DOMAIN + '/wp-admin/admin-ajax.php'
